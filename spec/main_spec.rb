76fdee8ff868a523ce48313a6ed8aa07f28dd5b2
Stefan Horning <stefan@hornings.de>
2015-09-03 12:36:01 +0200

    Some modifications in defaults and nginx.conf template

---
+++ spec/main_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Nginx setup" do
+  describe package('nginx') do
+    it { should be_installed }
+  end
+
+  describe service('nginx') do
+    it { should be_enabled }
+  end
+
+  describe file('/etc/nginx/nginx.conf') do
+    it { should be_file }
+    it { should contain ANSIBLE_VARS.fetch('nginx_http_params', 'FAIL').join(";\n") }
+  end
+
+  describe file('/etc/nginx/sites-available/') do
+    it { should be_directory }
+  end
+
+  describe file('/etc/nginx/sites-enabled/') do
+    it { should be_directory }
+  end
+
+  ANSIBLE_VARS.fetch('nginx_sites', nil).each do |name, config|
+    describe file("/etc/nginx/sites-available/#{name}.conf") do
+      it { should be_file }
+      it { should contain "#{config.join(";\n")}" }
+    end
+  end
+
+  ANSIBLE_VARS.fetch('nginx_sites', nil).each do |name, config|
+    describe file("/etc/nginx/sites-enabled/#{name}.conf") do
+      it { should be_symlink }
+    end
+  end
+
+  describe file("#{ANSIBLE_VARS.fetch('nginx_log_dir', 'FAIL')}/") do
+    it { should be_directory }
+  end
+end
