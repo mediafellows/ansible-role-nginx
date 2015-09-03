5a2fa9a438799894f036e91cff3acb8e6049463f
Stefan Horning <stefan@hornings.de>
2015-09-03 16:46:18 +0200

    Extended serverspecs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -10,6 +10,7 @@ describe "Nginx setup" do
 
   describe service('nginx') do
     it { should be_enabled }
+    it { should be_running }
   end
 
   describe file('/etc/nginx/nginx.conf') do
@@ -23,6 +24,14 @@ describe "Nginx setup" do
 
   describe file('/etc/nginx/sites-enabled/') do
     it { should be_directory }
+  end
+
+  describe file('/etc/nginx/sites-enabled/default') do
+    it { should_not exist }
+  end
+
+  describe file('/etc/nginx/conf.d/default.conf') do
+    it { should_not exist }
   end
 
   ANSIBLE_VARS.fetch('nginx_sites', nil).each do |name, config|
@@ -40,5 +49,15 @@ describe "Nginx setup" do
 
   describe file("#{ANSIBLE_VARS.fetch('nginx_log_dir', 'FAIL')}/") do
     it { should be_directory }
+    it { should be_owned_by('www-data') }
+  end
+
+  # NOT really dynamic (hardcoded filename) if params change!
+  describe file('/var/log/nginx/access.log') do
+    it { should be_file }
+  end
+
+  describe file('/var/log/nginx/error.log') do
+    it { should be_file }
   end
 end
